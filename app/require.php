04e7364c61a46acb66611daf8d3beec3a69afb14
Mohammad <alhaj.mhmd@gmail.com>
2020-11-28 13:25:10 +0800

    init

---
+++ app/require.php
@@ -0,0 +1,11 @@
+<?php
+// require libraries
+require_once 'config/config.php';
+require_once 'libraries/Database.php';
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+
+
+
+
+$init = new Core();
---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php 
+ foreach($data["users"] as $user){
+     echo 'this is user ' . $user->name;
+ }
